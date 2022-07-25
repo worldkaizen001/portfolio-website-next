b8429354b749a54cf314479f30a930a04ded7087
Aigbe Kelvin <86000836+worldkaizen001@users.noreply.github.com>
2022-07-25 13:37:03 +0100

    update

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,25 @@ export default function Document() {
           name="description"
           content="A portolio website of an outgoing developer called Kaizen"
         />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="icon" href="/favicon.ico"></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest" />
         <link
           rel="preload"
           href="https://www.aigbekelvin.tech/blackbg1.avif"
@@ -28,7 +42,7 @@ export default function Document() {
           href="https://www.aigbekelvin.tech/officestup.avif"
           as="image"
         ></link>
-        
+
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
