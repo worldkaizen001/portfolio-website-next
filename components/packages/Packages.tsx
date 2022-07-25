8215581bd5010c3c7d098e1225e61a5afec2ec92
Aigbe Kelvin <86000836+worldkaizen001@users.noreply.github.com>
2022-07-25 12:17:21 +0100

    update - v-1.0

--- components/packages/Packages.tsx
+++ components/packages/Packages.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { projects } from "../constants/projects";
+import { packages } from "../constants/packages";
 
 export default function Packages() {
   return (
     <section
       id="packages"
-      className="projects text-[#ffffff] bg-[#070707] pt-[3.3rem] 2xs:pt-[4.4rem] sm:pt-[5.5rem] md:pt-[6.6rem] xl:pt-[7.7rem] pb-[7.6rem] 2xs:pb-[7.8rem] sm:pb-[10.8rem] xl:pb-[13.7rem]"
+      className="packages bg-[#e5e7eb] pt-[3.3rem] 2xs:pt-[4.4rem] sm:pt-[5.5rem] md:pt-[6.6rem] xl:pt-[7.7rem] pb-[7.6rem] 2xs:pb-[7.8rem] sm:pb-[10.8rem] xl:pb-[13.7rem]"
     >
       <div className="container ">
         <div className="text-[1.45rem] 2xs:text-[1.6rem] xl:text-[1.8rem] pt-[.3rem] pb-[1.4rem]  font-normal border-b border-white ">
-          Projects *
+          Packages *
         </div>
         {packages?.map((val, idx) => (
           <div
@@ -27,7 +27,7 @@ export default function Packages() {
             </div>
             <div className="">
               <span className=" text-[1rem] 2xs:text-[1.1rem] sm:text-[1.25rem] md:text-[1.48rem] xl:text-[1.6rem] font-medium">
-                {val.industryType}
+                {val.community}
               </span>
               <br />
               <a target="_blank" rel="noopener noreferrer" href={val.link}>
